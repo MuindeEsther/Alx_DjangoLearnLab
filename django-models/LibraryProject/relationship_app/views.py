d1b6b4b263cc99f2a1717c1756decd4c23be9c42
MuindeEsther <estherndunge2000@gmail.com>
2025-11-07 15:01:22 +0300

    Created Function- Based View and Class-Based View

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,5 +14,5 @@ def book_list(request):
 # Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
