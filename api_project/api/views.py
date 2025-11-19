ceaf58e6a44b404c0d45d91626ebb70daeab4cad
MuindeEsther <estherndunge2000@gmail.com>
2025-11-19 12:46:18 +0300

    Add authentication and permission classes to BookViewSet; implement IsAdminOrReadOnly permission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+from .permissions import IsAdminOrReadOnly
 from rest_framework.views import APIView
 from .serializers import BookSerializer
 from rest_framework import generics
@@ -8,6 +11,14 @@ from rest_framework import viewsets
 # Create your views here.
 
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminOrReadOnly] # Only logged in users
+    
+    def get_permissions(self):
+        if self.action == 'destroy':   # DELETE
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 class BookList(generics.ListAPIView):
