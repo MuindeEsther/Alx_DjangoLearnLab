e5b7c9a8102eccd76714f4ee95c9c85398b547a2
MuindeEsther <estherndunge2000@gmail.com>
2025-11-19 12:35:06 +0300

    Implement BookViewSet and update URL routing for API endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,14 @@ from .models import Book
 from rest_framework.views import APIView
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework import viewsets
 
 # Create your views here.
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 class BookList(generics.ListAPIView):
-    queryset = Book.objects.all
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -23,5 +23,5 @@ from .views import home
 urlpatterns = [
     path('', home, name='home'), 
     path("admin/", admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/books/', include('api.urls')),
 ]
