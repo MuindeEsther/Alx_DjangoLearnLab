7332999a46b85d966c41cd232dbc8efbf5dcbd0a
MuindeEsther <estherndunge2000@gmail.com>
2025-11-24 15:47:42 +0300

    Added custom serializers

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -11,7 +11,6 @@ class Author(models.Model):
     
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
     published_year = models.DateField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
     
