c140ff4d8a40ceb0677b6bc64d867c39ffd66722
MuindeEsther <estherndunge2000@gmail.com>
2025-11-25 14:21:46 +0300

    Created a book model and run migrations

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+
+# Each author can have multiple books (one-to-many relationship).
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+# Linked to the Author model through a ForeignKey.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
+
